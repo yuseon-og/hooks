404c857a1e8c57e7dfda4c4b9431cd8b0a5f12a3
Yuseon <itsyuseon@gmail.com>
2021-03-17 16:41:08 +0900

    at office first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = React.useState(0);
+
+  const increse = () => {
+    tack.current.focus();
+    return setNumber(number + 1);
+  };
+
+  const decrese = () => setNumber(number - 1);
+  const sayHello = () => console.log("yeah");
+
+  React.useEffect(sayHello, [number]);
+
+  const tack = React.useRef();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> Test</div>
+      <div>{number}</div>
+      <button onClick={increse} ref={tack}>
+        올라감
+      </button>
+      <button onClick={decrese}>내려감</button>
     </div>
   );
-}
+};
+
+// class AppFucking extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       item: 0,
+//     };
+//   }
+
+//   render() {
+//     const {item} = this.state;
+//     return (
+//       <div className="App">
+//         <div> Test</div>
+//         <div>{item}</div>
+//         <button onClick={this.increse2}>올라감</button>
+//         <button onClick={this.decrese2}>내려감</button>
+//       </div>
+//     );
+//   }
+
+//   increse2 = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item + 1,
+//       };
+//     });
+//   };
+
+//   decrese2 = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item - 1,
+//       };
+//     });
+//   };
+// }
 
 export default App;
